1dce74373b010119f2aa3bb54d970e9fc6d013ee
Ash <ashton.naidoo@1nebula.com>
2021-10-06 18:24:33 +0200

    add next checks

--- CodeSubmissionSimple/Client/ClientTestEnv/Models/SubmissionSample.cs
+++ CodeSubmissionSimple/Client/ClientTestEnv/Models/SubmissionSample.cs
@@ -13,7 +13,7 @@ namespace CodeSubmissionSimple.Client.ClientTestEnv.Models
         public int QuestionNumber { get; set; }
         public string QuestionTitle { get; set; }
         public string QuestionDescription { get; set; }
-
+        
         [Required]
         public string Answer { get; set; }
         
